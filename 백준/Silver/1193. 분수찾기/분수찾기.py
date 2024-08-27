b661e67b3621aba6e9d38846def08af9cfeafeb2
Kyungwon Jeon <96944509+jeonkyungwon@users.noreply.github.com>
2024-08-27 17:34:52 +0900

    [Silver V] Title: 분수찾기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+X = int(input())
+n = 1
+
+while X > n:
+    X -= n
+    n += 1
+
+if n % 2 == 0:
+    a = X
+    b = n - X + 1
+else:
+    a = n - X + 1
+    b = X
+
+print(f"{a}/{b}")
