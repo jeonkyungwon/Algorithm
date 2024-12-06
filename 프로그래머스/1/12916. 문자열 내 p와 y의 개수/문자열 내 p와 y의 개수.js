02be9204f0dd7cd16df6f1f4cdba47fa106a7f2e
Kyungwon Jeon <96944509+jeonkyungwon@users.noreply.github.com>
2024-12-06 23:16:05 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -0,0 +1,19 @@
+function solution(s){
+    let p_cnt = 0;
+    let y_cnt = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == 'p' || s[i] == 'P') {
+            p_cnt += 1
+        }
+        if (s[i] == 'y' || s[i] == 'Y') {
+            y_cnt += 1
+        }
+    }
+
+    if (p_cnt === y_cnt) {
+        return true;
+    } else {
+        return false;
+    }
+}
