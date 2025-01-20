c73cce0140e410efc607660a33e60902a3961bc9
Kyungwon Jeon <96944509+jeonkyungwon@users.noreply.github.com>
2025-01-20 17:07:11 +0900

    [level 2] Title: [1차] 캐시, Time: 74.12 ms, Memory: 17.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+def solution(cacheSize, cities):
+    answer = 0
+    cache = deque()
+    
+    if cacheSize == 0:
+        return len(cities) * 5
+    
+    for city in cities:
+        city = city.upper()
+        
+        if city in cache:
+            cache.remove(city)
+            cache.append(city)
+            answer += 1
+        else:
+            if len(cache) < cacheSize:
+                cache.append(city)
+            else:
+                cache.popleft()
+                cache.append(city)
+            answer += 5
+            
+    return answer
