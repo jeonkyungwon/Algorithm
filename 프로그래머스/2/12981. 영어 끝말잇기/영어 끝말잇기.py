895f4a4ab8f906ce4735ab4f269c96e40b1c6ba0
Kyungwon Jeon <96944509+jeonkyungwon@users.noreply.github.com>
2025-02-07 15:03:27 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -4,13 +4,8 @@ def solution(n, words):
     
     for i in range(1, len(words)):
         if words[i] in answer or words[i - 1][-1] != words[i][0] :
-            return [person + 1, cnt + 1]
-        else:
-            answer.append(words[i])
+            return [(i % n) + 1, (i // n) + 1]
         
-        person += 1
-        if person == n:
-            person = 0
-            cnt += 1
+        answer.append(words[i])
     
     return [0, 0]
