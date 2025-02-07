0f6ba64d4863c95e6aa78c64719fcffcfa5fc300
Kyungwon Jeon <96944509+jeonkyungwon@users.noreply.github.com>
2025-02-07 15:03:47 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,6 +1,5 @@
 def solution(n, words):
     answer = [words[0]]
-    cnt, person = 0, 1
     
     for i in range(1, len(words)):
         if words[i] in answer or words[i - 1][-1] != words[i][0] :
