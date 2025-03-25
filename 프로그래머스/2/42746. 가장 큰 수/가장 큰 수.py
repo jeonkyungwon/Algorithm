6328bc89bd29e3cb145fcc098c11dd1abceb5455
Kyungwon Jeon <96944509+jeonkyungwon@users.noreply.github.com>
2025-03-25 17:01:13 +0900

    가장 큰 수 문제 풀이 방법 정리

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -4,3 +4,6 @@ def solution(numbers):
     answer = ''.join(str_numbers)
     
     return '0' if answer[0] == '0' else answer
+
+# 한 자리 문자열과 두 자리 이상의 문자열을 비교할 땐
+# x*3을 이용하여 숫자를 늘려 비교 기준을 맞춰서 비교한다.
