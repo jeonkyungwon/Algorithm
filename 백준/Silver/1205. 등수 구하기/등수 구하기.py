36e07547a9a9fcf90ee7408209f4f58a96593b30
Kyungwon Jeon <96944509+jeonkyungwon@users.noreply.github.com>
2025-04-30 17:29:29 +0900

    [Silver IV] Title: 등수 구하기, Time: 56 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.py
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.py
@@ -5,23 +5,14 @@ input = sys.stdin.readline
 N, score, P = map(int, input().split())
 rank = list(map(int, input().split()))
 
-if len(rank) == P:
-    if score <= min(rank):
+if len(rank) == P and score <= min(rank):
         print(-1)
-    else:
-        rank.pop()
-        rank.append(score)
-        rank.sort(reverse=True)
-        answer = 1
-        for i in range(len(rank)):
-            if rank[i] == score:
-                print(answer)
-                break
-            else:
-                answer += 1
+
 else:
     rank.append(score)
     rank.sort(reverse=True)
+    if len(rank) > P:
+        rank.pop()
 
     answer = 1
     for i in range(len(rank)):
