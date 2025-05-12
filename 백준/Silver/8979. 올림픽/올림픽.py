46c4292b12423d700b98c1ea5c97e672f9aa4edb
Kyungwon Jeon <96944509+jeonkyungwon@users.noreply.github.com>
2025-05-12 17:44:13 +0900

    [Silver V] Title: 올림픽, Time: 36 ms, Memory: 33432 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -2,19 +2,24 @@ import sys
 input = sys.stdin.readline
 
 def check_rank(country):
-    rank = 1
+    rank, cnt = 1, 1
 
     for i in range(N - 1):
         if country[i][0] == K:
             return rank
         if country[i][1] > country[i + 1][1]:
-            rank += 1
+            rank += cnt
+            cnt = 1
         elif country[i][1] == country[i + 1][1]:
             if country[i][2] > country[i + 1][2]:
-                rank += 1
+                rank += cnt
+                cnt = 1
             elif country[i][2] == country[i + 1][2]:
                 if country[i][3] > country[i + 1][3]:
-                    rank += 1
+                    rank += cnt
+                    cnt = 1
+                elif country[i][3] == country[i + 1][3]:
+                    cnt += 1
 
     return rank
 
