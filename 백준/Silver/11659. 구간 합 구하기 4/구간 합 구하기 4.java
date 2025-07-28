b327a1c2c289fc5caeb2df3817d159c8589976f9
Kyungwon Jeon <96944509+jeonkyungwon@users.noreply.github.com>
2025-07-28 15:52:53 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1160 ms, Memory: 63900 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,29 +1,40 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		int N = scan.nextInt();
-		int M = scan.nextInt();
+	static int[] nums, prefix;
+	
+    public static void main(String[] args) throws IOException {        
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	
+    	int N = Integer.parseInt(st.nextToken());
+    	int M = Integer.parseInt(st.nextToken());
+    	
+    	nums = new int[N];
+    	prefix = new int[N];
+    	
+    	st = new StringTokenizer(br.readLine());
+    	for (int n = 0; n < N; n++) {
+    		nums[n] = Integer.parseInt(st.nextToken());
+    	}
+    	
+    	prefix[0] = nums[0];
+    	for (int n = 1; n < N; n++) {
+    	    prefix[n] = prefix[n - 1] + nums[n];
+    	}
+    	
+    	for (int m = 0; m < M; m++) {
+        	st = new StringTokenizer(br.readLine());
 
-	    int[] numArry = new int[N + 1];
-	    for (int i = 1; i < N + 1; i++) {
-	    	numArry[i] = scan.nextInt();
-		}
-	    
-	    int StackSum = 0;
-	    int[] SumArry = new int[N + 1];
-	    for (int i = 1; i < N + 1; i++) {
-	    	StackSum += numArry[i];
-	    	SumArry[i] = StackSum;
-	    }
-	    
-	    for (int a = 0; a < M; a++) {
-	    	int i = scan.nextInt();
-	    	int j = scan.nextInt();
-	    	
-	    	System.out.println(SumArry[j] - SumArry[i - 1]);
-	    }
-	}
+    		int i = Integer.parseInt(st.nextToken()) - 1;
+    		int j = Integer.parseInt(st.nextToken()) - 1;
+    		
+            int sum = i == 0 ? prefix[j] : prefix[j] - prefix[i - 1];
+            System.out.println(sum);
+    	}
+    }
 }
