e3079509693c80d36b9b6eb6ed9bbfa031fc18c5
Kyungwon Jeon <96944509+jeonkyungwon@users.noreply.github.com>
2025-08-12 12:08:18 +0900

    [Silver I] Title: 숨바꼭질, Time: 136 ms, Memory: 19608 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N, K;
+	
+    public static void main(String[] args) throws IOException {        
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	
+    	N = Integer.parseInt(st.nextToken());
+    	K = Integer.parseInt(st.nextToken());
+    	
+    	System.out.println(bfs(N, K));
+    }
+    
+    static int bfs(int start, int target) {
+    	if (start > target) return start - target;
+
+    	int[] dist = new int[100001];
+    	Arrays.fill(dist, -1);
+    	
+    	Queue<Integer> q = new ArrayDeque<>();
+    	dist[start] = 0;
+    	q.offer(start);
+    	
+    	while (!q.isEmpty()) {
+    		int x = q.poll();
+    		int[] nexts = {x - 1, x + 1, x * 2};
+    		
+    		if (x == target) return dist[x];
+    		
+    		for (int nx : nexts) {
+    			if (nx < 0 || nx > 100000) continue;
+    			if (dist[nx] != -1) continue;
+    			dist[nx] = dist[x] + 1;
+    			q.offer(nx);
+    		}
+    		
+    	}
+    	
+    	return -1;
+    }
+}
