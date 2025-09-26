14a6d007fd0bb9524e0035ae8e41872811a635dd
Kyungwon Jeon <96944509+jeonkyungwon@users.noreply.github.com>
2025-09-26 09:23:38 +0900

    [Silver V] Title: 돌 게임, Time: 108 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	int N = Integer.parseInt(br.readLine());
+    	
+    	int cnt = 0;		
+    	while (N > 0) {
+    		if (N - 3 > 0) N -= 3; 
+    		else N--;
+    		cnt++;
+    	}
+
+    	System.out.println(cnt % 2 == 1 ? "SK" : "CY");
+    }
+}
