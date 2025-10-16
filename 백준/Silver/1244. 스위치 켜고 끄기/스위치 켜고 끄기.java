16a1f926c01e4818e1ba1d063bc08df393525bbb
Kyungwon Jeon <96944509+jeonkyungwon@users.noreply.github.com>
2025-10-16 15:50:15 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,70 +1,62 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int N = scan.nextInt();
-		
-		int[] Switch = new int[N];
-		for(int i = 0; i < N; i++) {
-			Switch[i] = scan.nextInt();
-		}
+	
+	static int[] switches;
+	
+	public static void main(String[] args) throws IOException {        
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	int N = Integer.parseInt(br.readLine());
+    	switches = new int[N + 1];
+    	
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	for (int n = 1; n <= N; n++) {
+    		switches[n] = Integer.parseInt(st.nextToken());
+    	}
 
-		int M = scan.nextInt();
+    	int M = Integer.parseInt(br.readLine());
+    	for (int m = 0; m < M; m++ ) {
+    		st = new StringTokenizer(br.readLine());
+    		int s = Integer.parseInt(st.nextToken());
+    		int c = Integer.parseInt(st.nextToken());
+    		
+    		if (s == 1) {
+    			for (int i = 1; c * i <= N; i++) {
+    				if (switches[c * i] == 0) switches[c * i] = 1;
+    				else switches[c * i] = 0;
+    			}
+    		} else {
+    			if (switches[c] == 0) switches[c] = 1;
+				else switches[c] = 0;    			
 
-		for(int i = 0; i < M; i++) {
-			int S = scan.nextInt();
-			int num = scan.nextInt();
-			
-			if(S == 1) {
-				for(int j = 0; j < N; j++) {
-					if((j + 1) % num == 0) {
-						if(Switch[j] == 0) {
-							Switch[j] = 1;
-						}
-						else {
-							Switch[j] = 0;
-						}
-					}
-				}
-			}
-			else {
-				int j = 1;
-				
-				if(Switch[num - 1] == 0) {
-					Switch[num - 1] = 1;
-				}
-				else {
-					Switch[num -1 ] = 0;
-				}
-				if((num - 1 - j >= 0) && (num - 1 + j < N)) {
-					while (Switch[num - 1 - j] == Switch[num - 1 + j]) {
-						if(Switch[num - 1 - j] == 0) {
-							Switch[num - 1 - j] = 1;
-						}
-						else {
-							Switch[num - 1 - j] = 0;
-						}
-						if(Switch[num - 1 + j] == 0) {
-							Switch[num - 1 + j] = 1;
-						}
-						else {
-							Switch[num - 1 + j] = 0;
-						}
-						j += 1;
-						if((num - 1 - j < 0) || (num - 1 + j >= N)) {
-							break;
-						}
-					}
-				}
-			}
-		}
-		
-		for(int i = 0; i < N; i++) {
-			if(i != 0 && i % 20 == 0) {
-				System.out.println();
-			}
-			System.out.print(Switch[i] + " ");
-		}
-	}
+    			int i = 1;
+    			while (c - i > 0 && c + i <= N) {
+    				if (switches[c - i] == switches[c + i]) {
+    					if (switches[c - i] == 0) {
+    						switches[c - i] = 1;
+    						switches[c + i] = 1;
+    					}
+        				else {
+        					switches[c - i] = 0;
+    						switches[c + i] = 0;
+        				}
+    				} else {
+    					break;
+    				}
+    				
+    				i++;
+    			}
+    		}
+    	}
+    	
+    	StringBuilder sb = new StringBuilder();
+    	for (int n = 1; n <= N; n++) {
+    		sb.append(switches[n]).append(" ");
+    		if (n % 20 == 0) sb.append("\n");
+    	}
+    	
+    	System.out.println(sb.toString());
+    }
 }
