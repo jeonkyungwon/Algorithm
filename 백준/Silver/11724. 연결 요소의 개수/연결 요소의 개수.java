40d736b5587b484014eb545d70ea3a756eb5e354
Kyungwon Jeon <96944509+jeonkyungwon@users.noreply.github.com>
2025-10-22 17:34:46 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 620 ms, Memory: 142356 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,45 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
+
 	static List<List<Integer>> graph = new ArrayList<>();
 	static boolean[] visited;
-	static int cnt = 0;
 	
-    public static void main(String[] args) throws IOException {        
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	
-    	int N = Integer.parseInt(st.nextToken());
-    	int M = Integer.parseInt(st.nextToken());
-    	
-    	for (int i = 0; i <= N; i++ ) {
-    		graph.add(new ArrayList<>());
-    	}
-    	
-    	for (int m = 0; m < M; m++) {
-    		st = new StringTokenizer(br.readLine());
-    		int u = Integer.parseInt(st.nextToken());
-    		int v = Integer.parseInt(st.nextToken());
-    		
-    		graph.get(u).add(v);
-    		graph.get(v).add(u);
-    	}
-    	
-    	visited = new boolean[N + 1];
-    	for (int i = 1; i <= N; i++) {
-    		if (!visited[i]) {
-    			cnt++;
-    			dfs(i);
-    		}
-    	}
-    	
-    	System.out.println(cnt);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        for (int n = 0; n <= N; n++) {
+        	graph.add(new ArrayList<>());
+        }
+        
+        for (int m = 0; m < M; m++) {
+        	st = new StringTokenizer(br.readLine());
+        	int u = Integer.parseInt(st.nextToken());
+        	int v = Integer.parseInt(st.nextToken());
+        	
+        	graph.get(u).add(v);
+        	graph.get(v).add(u);
+        }
+        
+        visited = new boolean[N + 1];
+        int cnt = 0;
+        for (int i = 1; i <= N; i++) {
+        	if (!visited[i]) {
+        		dfs(i);
+        		cnt++;
+        	}
+        }
+        
+        System.out.println(cnt);
     }
     
     static void dfs(int node) {
@@ -47,7 +43,8 @@ public class Main {
     	
     	for (int next : graph.get(node)) {
     		if (!visited[next]) {
-        		dfs(next);	
+    			visited[next] = true;
+    			dfs(next);
     		}
     	}
     }
