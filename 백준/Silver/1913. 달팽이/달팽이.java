07638dc0f593be52d098848dc6ae26fe9c0b4834
Kyungwon Jeon <96944509+jeonkyungwon@users.noreply.github.com>
2026-01-08 09:46:49 +0900

    [Silver III] Title: 달팽이, Time: 464 ms, Memory: 49024 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.java
+++ 백준/Silver/1913. 달팽이/달팽이.java
@@ -1,67 +1,74 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	static int N, L;
-	static int[] dx = {-1, 0, 1, 0};
-	static int[] dy = {0, 1, 0, -1};
-	static int[] location = new int[2];
-	
-	public static void main(String[] args) throws IOException {		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		N = Integer.parseInt(br.readLine());
-		L = Integer.parseInt(br.readLine());
-		int [][] snail = generateSnail();
-		
-		printSnail(snail);
-	}
-	
-	static int[][] generateSnail() {
-		int [][] snail = new int[N][N];
-		int x = N / 2, y = N / 2;
-		int num = 1, d = 0;
-		int cnt = 0, step = 1, s = 0;
-		
-		while (num <= N * N) {
-			snail[x][y] = num;
-			if (num == L) {
-				location[0] = x + 1;
-				location[1] = y + 1;
-			}
-			
-			x += dx[d];
-			y += dy[d];
-			s += 1;
-			num += 1;
-			
-			if (s == step) {
-				d = (d + 1) % 4;
-				s = 0;
-				cnt += 1;
-			}
-			
-			if (cnt == 2 && step < N - 1) {
-				step += 1;
-				cnt = 0;
-			}
-		}
-		
-		return snail;
-	}
-	
-	static void printSnail(int[][] snail) {
-		StringBuilder sb = new StringBuilder();
-		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				sb.append(snail[i][j]).append(" ");
-			}
-			sb.append("\n");
-		}		
-		System.out.print(sb);
-		System.out.println(location[0] + " " + location[1]);
-	}
+
+    static int N, M;
+    static int[] dr = {-1, 0, 1, 0}, dc = {0, 1, 0, -1};
+    static int[] target;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+
+        int[][] snail = CreateSnail();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(snail[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+
+        sb = new StringBuilder();
+        for (int i = 0; i < 2; i++) {
+            sb.append(target[i]).append(" ");
+        }
+        System.out.println(sb);
+    }
+
+    static int[][] CreateSnail() {
+        int[][] snail = new int[N][N];
+        int r = N / 2, c = N / 2;
+        int i = 1, d = 0, j = 1, cnt = 0;
+
+        target = new int[2];
+        snail[r][c] = 1;
+        if (i == M) {
+            target[0] = r + 1;
+            target[1] = c + 1;
+        }
+        i++;
+
+        while (i <= N * N) {
+            for (int a = 0; a < j; a++) {
+                r += dr[d % 4];
+                c += dc[d % 4];
+
+                if (i == M) {
+                    target[0] = r + 1;
+                    target[1] = c + 1;
+                }
+
+                snail[r][c] = i;
+                i++;
+
+                if (i > N * N) {
+                    break;
+                }
+            }
+            cnt++;
+            d++;
+
+            if (cnt == 2) {
+                cnt = 0;
+                j++;
+            }
+        }
+
+        return snail;
+    }
 }
