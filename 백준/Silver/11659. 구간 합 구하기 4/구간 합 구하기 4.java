7d44ee0921ddd64e7c4f5ce9834904c3d0eb5d8b
Kyungwon Jeon <96944509+jeonkyungwon@users.noreply.github.com>
2026-01-26 20:47:58 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 576 ms, Memory: 54800 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,40 +1,34 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int[] nums, prefix;
-	
-    public static void main(String[] args) throws IOException {        
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	
-    	int N = Integer.parseInt(st.nextToken());
-    	int M = Integer.parseInt(st.nextToken());
-    	
-    	nums = new int[N];
-    	prefix = new int[N];
-    	
-    	st = new StringTokenizer(br.readLine());
-    	for (int n = 0; n < N; n++) {
-    		nums[n] = Integer.parseInt(st.nextToken());
-    	}
-    	
-    	prefix[0] = nums[0];
-    	for (int n = 1; n < N; n++) {
-    	    prefix[n] = prefix[n - 1] + nums[n];
-    	}
-    	
-    	for (int m = 0; m < M; m++) {
-        	st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-    		int i = Integer.parseInt(st.nextToken()) - 1;
-    		int j = Integer.parseInt(st.nextToken()) - 1;
-    		
-            int sum = i == 0 ? prefix[j] : prefix[j] - prefix[i - 1];
-            System.out.println(sum);
-    	}
+        int[] nums = new int[N];
+        int[] sums = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int n = 0; n < N; n++) {
+            nums[n] = Integer.parseInt(st.nextToken());
+            if (n == 0) sums[n] = nums[n];
+            else sums[n] = sums[n - 1] + nums[n];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int m = 0; m < M; m++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+
+            if (i == 1) sb.append(sums[j - 1]);
+            else sb.append(sums[j - 1] - sums[i - 2]);
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
     }
 }
